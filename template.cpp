c968da1caa80e01ddd853e1e79849325245d3c01
Nelson Taveras <ntaveras03@gmail.com>
2014-05-14 00:41:41 -0400

    Added C++ template

---
+++ template.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <cstring>
+#include <queue>
+#include <stack>
+#include <sstream>
+#include <cstdio>
+
+using namespace std;
+
+#define mp make_pair
+typedef long long llong;
+typedef unsigned long long ullong;
+typedef pair<int, int> PI;
+typedef pair<PI, int> PII;
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+llong INF = (1LL<<62);
+const int inf = (1LL<<30);
+const int maxn = 100010;
+
+int main(){
+	return 0;
+}
